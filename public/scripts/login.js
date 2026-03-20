a3e2cd76381e33a64c691171b12d7799f9c4d357
ReneLaverdure <renelaverdure@gmail.com>
2026-03-21 01:00:23 +1100

    frontend login and rooms

---
+++ public/scripts/login.js
@@ -0,0 +1,22 @@
+console.log("login");
+
+const form = document.querySelector("form");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const data = {
+    username: e.target.username.value,
+    password: e.target.password.value,
+  };
+  console.log(data);
+  const response = await fetch("/api/auth", {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  const result = await response.json();
+  console.log(result);
+  localStorage.setItem("userID", result.id);
+  if (result.id) {
+    window.location.href = "http://localhost:3000/";
+  }
+});
+console.log(form, "asdf");
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,11 @@
 console.log("front end");
+const url = "http://localhost:3000/api/auth";
+
+async function getAuth() {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+const data = await getAuth();
+console.log(data);
